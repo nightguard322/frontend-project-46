74ecafeaa308812e0f8401139205213a02d89e9a
Your Name <you@example.com>
2026-03-02 15:40:42 +0300

    fixed main exec

--- src/parsers/yaml.js
+++ src/parsers/yaml.js
@@ -1,16 +1,16 @@
 import * as path from 'path'
 import * as fs from 'fs'
+import yaml from 'js-yaml'
 
 export const parser = (filepath) => {
   try {
     const fullpath = path.resolve(filepath)
     const file = fs.readFileSync(fullpath, 'utf-8')
-    return JSON.parse(file)
+    return yaml.load(file)
   }
   catch (err) {
-    console.log('Пробуем парсить yaml по пути:', filepath, 'но с файлом проблем!')
-    if (err instanceof SyntaxError) {
-      throw new Error(`file ${filepath} is not a valid JSON`, { cause: err })
+    if (err instanceof yaml.YAMLException) {
+      throw new Error(`file ${filepath} is not a valid YAML`, { cause: err })
     }
   }
 }
